9032ae5e22762906644c52e3fbd2e3eafb291e16
dymhymn16-ui <dymhymn16@gmail.com>
2026-09-03 01:27:44 +0300

    Update navigation-router.tsx

--- src/routers/navigation-router.tsx
+++ src/routers/navigation-router.tsx
@@ -75,7 +75,10 @@ export const NavigationRouter = React.memo(() => {
     animationDuration: 350,
     animation: 'flip',
   }}
-    <"initialRouteName="Initiation
+  initialRouteName="Initiation"
+>
+  
+    
               <Stack.Screen name="Main" component={TabBarNavigation} />
               <Stack.Screen name="Error" component={ErrorScreen} />
               <Stack.Screen name="Initiation" component={InitiationScreen} />
